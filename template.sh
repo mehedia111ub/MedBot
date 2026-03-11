388467b884df06c8ea40a411427892588a5c3af3
mflix24 <banglacourse1223@gmail.com>
2026-03-11 20:28:41 +0000

    Project Structure Added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Creating Directories
+mkdir -p src
+mkdir -p research
+
+# Creating Files0
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+echo "Directory and Files created successfully"
+
+
+
